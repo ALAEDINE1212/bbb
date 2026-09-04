6a25d346243e8ab1679cbf830bb8c633097359f5
Alaedine Ait bella <aaitbell@bradford.ac.uk>
2026-09-04 17:39:07 +0100

    api fix

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,9 +2,8 @@
 importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging-compat.js');
 
-// Your exact Firebase config
 firebase.initializeApp({
-  apiKey: "AIzaSyBG2JNN8K6x_W_wLlJawc0Gwk08ixwJSHM",
+  apiKey: "AIzaSyBG2NJN8K6x_W_wLlJawc0Gwk08ixwJSHM", // <-- Updated key here
   authDomain: "my-baby-35bb9.firebaseapp.com",
   projectId: "my-baby-35bb9",
   storageBucket: "my-baby-35bb9.firebasestorage.app",
@@ -14,7 +13,6 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// Listen for background messages
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   
